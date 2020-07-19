1a2b3c29f44db50958d27a816ed73c3cc92fb473
prana-magar <Prakashrana9847067614@gmail.com>
2020-07-19 17:16:44 -0400

    initial setup for map

--- app/src/main/java/com/example/tovisit_prakash_c0773839_android/MainActivity.java
+++ app/src/main/java/com/example/tovisit_prakash_c0773839_android/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.tovisit_prakash_c0773839_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+    FloatingActionButton btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.fabAddPlace);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
